8ae054f969d3f9f88115dc0baed4c21ec6d259f3
Valentin Lehmann <info@valentinlehmann.de>
2025-10-07 15:05:15 +0200

    Fix formatting in Package.swift

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "obs-websocket-client",
     platforms: [
-        .macOS(.v13)
+        .macOS(.v13),
         .iOS(.v26)
     ],
     products: [
